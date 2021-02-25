ac1402925dfb82e6af7afd229a4f31a36c0d55a0
Jing Wu <jingwu@JingdeMacBook-Pro.local>
2021-02-25 16:42:50 +0800

    calculateWinner

--- src/component/Board.js
+++ src/component/Board.js
@@ -9,6 +9,9 @@ const Board = () => {
   const [state, setState] = useState(initialState);
   const handleClick = (i) => {
     const squares = [...state.squares];
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = state.xIsNext ? 'X' : 'O';
     setState(
       {
@@ -16,6 +19,26 @@ const Board = () => {
         xIsNext: !state.xIsNext,
       }
     );
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   const renderSquare = (i) => {
@@ -26,7 +49,15 @@ const Board = () => {
       />
     )
   }
-  const status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+
+  const winner = calculateWinner(state.squares);
+  let status;
+  if (winner) {
+    status = "Winner is " + winner;
+  } else {
+    status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
